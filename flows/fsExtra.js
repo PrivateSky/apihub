1d09c61bd5b240229c0a8f60322915c8a90d6a8c
StefanCosminR <stefancosmin.romanescu@gmail.com>
2018-10-01 16:19:58 +0300

    Added flow to calculate folder size (useful fore quota)

---
+++ flows/fsExtra.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const fs = require("fs");
+
+
+$$.flow.create('fsExtra', {
+    getFolderSize: function (folder, callback) {
+        let total = 0;
+
+        folder = path.resolve(folder);
+
+        fs.readdir(folder, (err, files) => {
+            if (err) {
+                return callback(err);
+            }
+
+            let remainingFiles = files.length;
+
+            if (!remainingFiles) {
+                return callback(undefined, total);
+            }
+
+            for (let i = 0; i < files.length; ++i) {
+                const filePath = path.join(folder, files[i]);
+                fs.stat(filePath, (err, stats) => {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    if (stats.isDirectory()) {
+                        this.getFolderSize(filePath, (err, size) => {
+                            if (err) {
+                                console.error(err);
+                                return callback(err);
+                            }
+
+                            total += size;
+
+                            if (!--remainingFiles) {
+                                callback(undefined, total);
+                            }
+                        });
+
+                    } else {
+                        total += stats.size;
+
+
+                        if (--remainingFiles === 0) {
+
+                            callback(undefined, total);
+                        }
+                    }
+                });
+            }
+        });
+    }
+});
