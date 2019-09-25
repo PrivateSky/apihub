685ad413a66c9405483afbb949b73620c846b25a
asac <asac.cool@gmail.com>
2019-09-25 14:50:58 +0300

    send message endpoint

--- ChannelsManager.js
+++ ChannelsManager.js
@@ -1,8 +1,8 @@
-const storageFolder = process.env.channel_storage || "../tmp";
-const defaultQueueSize = process.env.queue_size || 100;
-const tokenSize = process.env.token_size || 48;
-const tokenHeaderName = process.env.token_header_name || "tokenHeader";
-const signatureHeaderName = process.env.signature_header_name || "signature";
+const storageFolder = process.env.vmq_channel_storage || "../tmp";
+const maxQueueSize = process.env.vmq_max_queue_size || 100;
+const tokenSize = process.env.vmq_token_size || 48;
+const tokenHeaderName = process.env.vmq_token_header_name || "tokenHeader";
+const signatureHeaderName = process.env.vmq_signature_header_name || "signature";
 
 const channelsFolderName = "channels";
 const channelKeyFileName = "channel_key";
@@ -11,12 +11,16 @@ const path = require("path");
 const fs = require("fs");
 const crypto = require('crypto');
 
+const Queue = require("swarmutils").Queue;
+
 function ChannelsManager(server){
 
     const rootFolder = path.join(storageFolder, channelsFolderName);
     fs.mkdirSync(rootFolder, {recursive: true});
 
     const channelKeys = {};
+    const queues = {};
+    const subscribers = {};
 
     function generateToken(){
         let buffer = crypto.randomBytes(tokenSize);
@@ -38,15 +42,16 @@ function ChannelsManager(server){
             return callback(new Error("channel has owner set!"));
         }
 
-        fs.writeFile(keyFile, JSON.stringify({publicKey, token}), (err, res)=>{
+        const config = JSON.stringify({publicKey, token});
+        fs.writeFile(keyFile, config, (err, res)=>{
             if(!err){
-                channelKeys[name] = publicKey;
+                channelKeys[name] = config;
             }
             return callback(err, !err ? token : undefined);
         });
     }
 
-    function retriveChannelKey(channelName, callback){
+    function retriveChannelDetails(channelName, callback){
         if(typeof channelKeys[channelName] !== "undefined"){
             return callback(null, channelKeys[channelName]);
         }else{
@@ -73,6 +78,7 @@ function ChannelsManager(server){
                 config.forward = forward;
                 fs.writeFile(channelKeyFile, JSON.stringify(config), (err, ...args)=>{
                     if(!err){
+                        channelKeys[channelName] = config;
                         //TODO: start forward client
                     }
                     callback(err, ...args);
@@ -126,7 +132,6 @@ function ChannelsManager(server){
         res.end();
     }
 
-
     function getBasicReturnHandler(res){
         return function(err, result){
             if(err){
@@ -148,11 +153,11 @@ function ChannelsManager(server){
                 return sendStatus(res, 400);
             }
 
-            retriveChannelKey(channelName, (err, key)=>{
+            retriveChannelDetails(channelName, (err, details)=>{
                 if(err){
                     return sendStatus(res, 500);
                 }else{
-                    //todo: check signature against key
+                    //todo: check signature against key [details.publickey]
 
                     if(typeof enable === "undefined" || enable){
                         forwardChannel(channelName, true, getBasicReturnHandler(res));
@@ -162,15 +167,77 @@ function ChannelsManager(server){
                 }
             });
         });
-
     });
 
-    server.post("/add-message/:channelName", function(req, res){
+    function getQueue(name){
+        if(typeof queues[name] === "undefined"){
+            queues[name] = new Queue();
+        }
 
+        return queues[name];
+    }
+
+    server.post("/send-message/:channelName", function(req, res){
+        let channelName = req.body.channelName;
+        readBody(req, (err, message)=>{
+            let queue = getQueue(channelName);
+            let subscribers = getSubscribersList(channelName);
+            let dispatched = false;
+            if(queue.isEmpty()){
+                try {
+                    for (let i = 0; i < subscribers.length; i++) {
+                        subscribers[i].write(message);
+                        sendStatus(subscribers[i], 200);
+                    }
+                    dispatched = true;
+                }catch(err) {
+                    //... some subscribers could have a timeout connection
+                }
+            }
+            if(!dispatched) {
+                if(queue.length < maxQueueSize){
+                    queue.push(message);
+                }else{
+                    return sendStatus(res, 429);
+                }
+
+                /*
+                if(subscribers.length>0){
+                    //... if we have somebody waiting for a message and the queue is not empty means that something bad
+                    //happened and maybe we should try to dispatch first message from queue
+                }
+                */
+
+            }
+            return sendStatus(res, 200);
+        });
     });
 
-    server.post("/receive-message/:channelName", function(req, res){
+    function getSubscribersList(channelName){
+        if(typeof subscribers[channelName] === "undefined"){
+            subscribers[channelName] = [];
+        }
 
+        return subscribers[channelName];
+    }
+
+    server.get("/receive-message/:channelName", function(req, res){
+        let channelName = req.params.channelName;
+        retriveChannelDetails(channelName, (err, details)=>{
+            if(err){
+                return sendStatus(res, 500);
+            }
+            //TODO: check signature agains details.publickey
+            let queue = getQueue(channelName);
+            let message = queue.pop();
+
+            if(!message){
+                getSubscribersList.push(res);
+            }else{
+                res.write(message);
+                sendStatus(res, 200);
+            }
+        });
     });
 
 }
