e6013a58e3b6aa9ea66e9c3513f60587ea13c417
asac <asac.cool@gmail.com>
2019-10-09 14:15:01 +0300

    test if callback present

--- VirtualMQ.js
+++ VirtualMQ.js
@@ -16,7 +16,9 @@ function VirtualMQ({listeningPort, rootFolder, sslConfig}, callback) {
 	let bindFinish = (err)=>{
 		if(err){
 			console.log(err);
-			callback(err);
+			if(callback){
+				callback(err);
+			}
 			return;
 		}
 		const tokenBucket = new TokenBucket(600000, 1, 10);
