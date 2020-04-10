6d39971201e396de9f973afadc178b3c16c820d0
Cosmin Ursache <asac.cool@gmail.com>
2020-04-10 19:36:39 +0300

    added endpoint to serve static files and added endpoint /directory-summary

--- FilesManager.js
+++ FilesManager.js
@@ -78,7 +78,7 @@ function download(req, res, callback) {
 	}
 }
 
-function sendResult(resHandler, resultStream){
+function sendResult(resHandler, resultStream) {
 	resHandler.statusCode = 200;
 	resultStream.pipe(resHandler);
 	resultStream.on('finish', () => {
@@ -86,7 +86,7 @@ function sendResult(resHandler, resultStream){
 	});
 }
 
-function FilesManager(server){
+function FilesManager(server) {
 	//folder can be userId/tripId/...
 	server.post('/files/upload/:folder', function (req, res) {
 		upload(req, (err, result) => {
@@ -111,6 +111,161 @@ function FilesManager(server){
 		});
 	});
 
+	const lockedPathsPrefixes = ["/EDFS", "/receive-message"];
+	if (typeof process.env.PSK_VIRTUAL_MQ_STATIC !== "undefined" && process.env.PSK_VIRTUAL_MQ_STATIC === "true") {
+		server.use("*", function (req, res, next) {
+			const prefix = "/directory-summary/";
+			requestValidation(req, "GET", prefix, function (notOurResponsibility, targetPath) {
+				if (notOurResponsibility) {
+					return next();
+				}
+				targetPath = targetPath.replace(prefix, "");
+				serverTarget(targetPath);
+			});
+
+			function serverTarget(targetPath) {
+				console.log("Serving summary for dir:", targetPath);
+				fs.stat(targetPath, function (err, stats) {
+					if (err) {
+						res.statusCode = 404;
+						res.end();
+						return;
+					}
+					if (!stats.isDirectory()) {
+						res.statusCode = 403;
+						res.end();
+						return;
+					}
+
+					function send() {
+						res.statusCode = 200;
+						res.setHeader('Content-Type', "application/json");
+						res.write(JSON.stringify(summary));
+						res.end();
+					}
+
+					let summary = {};
+					let directories = {};
+
+					function extractContent(currentPath) {
+						directories[currentPath] = -1;
+						let summaryId = currentPath.replace(server.rootFolder, "");
+						summaryId = summaryId.split(path.sep).join("/");
+						summary[summaryId] = {};
+
+						fs.readdir(currentPath, function (err, files) {
+							if (err) {
+								return markAsFinish(currentPath);
+							}
+							directories[currentPath] = files.length;
+							//directory empty test
+							if (files.length === 0) {
+								return markAsFinish(currentPath);
+							} else {
+								for (let i = 0; i < files.length; i++) {
+									let file = files[i];
+									const fileName = path.join(currentPath, file);
+									if (fs.statSync(fileName).isDirectory()) {
+										extractContent(fileName);
+									} else {
+										let fileContent = fs.readFileSync(fileName);
+										summary[summaryId][file] = fileContent.toString();
+									}
+									directories[currentPath]--;
+								}
+								return markAsFinish(currentPath);
+							}
+						});
+					}
+
+					function markAsFinish(targetPath) {
+						if (directories [targetPath] > 0) {
+							return;
+						}
+						delete directories [targetPath];
+						const dirsLeftToProcess = Object.keys(directories);
+						//if there are no other directories left to process
+						if (dirsLeftToProcess.length === 0) {
+							send();
+						}
+					}
+
+					extractContent(targetPath);
+				})
+			}
+
+		});
+
+		server.use("*", function (req, res, next) {
+			requestValidation(req, "GET", function (notOurResponsibility, targetPath) {
+				if (notOurResponsibility) {
+					return next();
+				}
+				//from now on we mean to resolve the url
+				fs.stat(targetPath, function (err, stats) {
+					if (err) {
+						res.statusCode = 404;
+						res.end();
+						return;
+					}
+					if (stats.isDirectory()) {
+						//we don't support yet directory serving or default files like index.html
+						res.statusCode = 403;
+						res.end();
+					} else {
+						let stream = fs.createReadStream(targetPath);
+						const mimes = require("./MimeType");
+						let ext = path.extname(targetPath);
+						if (ext !== "") {
+							ext = ext.replace(".", "");
+							res.setHeader('Content-Type', mimes.getMimeTypeFromExtension(ext).name);
+						} else {
+							res.setHeader('Content-Type', "application/octet-stream");
+						}
+						return sendResult(res, stream);
+					}
+				});
+			});
+		});
+
+		function requestValidation(req, method, urlPrefix, callback) {
+			if (typeof urlPrefix === "function") {
+				callback = urlPrefix;
+				urlPrefix = undefined;
+			}
+			if (req.method !== method) {
+				//we resolve only GET requests
+				return callback(true);
+			}
+
+			if (typeof urlPrefix === "undefined") {
+				for (let i = 0; i < lockedPathsPrefixes.length; i++) {
+					let reservedPath = lockedPathsPrefixes[i];
+					//if we find a url that starts with a reserved prefix is not our duty ro resolve
+					if (req.url.indexOf(reservedPath) === 0) {
+						return callback(true);
+					}
+				}
+			} else {
+				if (req.url.indexOf(urlPrefix) !== 0) {
+					return callback(true);
+				}
+			}
+
+			const rootFolder = server.rootFolder;
+			const path = require("path");
+			let requestedUrl = req.url;
+			if (urlPrefix) {
+				requestedUrl = requestedUrl.replace(urlPrefix, "");
+			}
+			let targetPath = path.resolve(path.join(rootFolder, requestedUrl));
+			//if we detect tricks that tries to make us go above our rootFolder to don't resolve it!!!!
+			if (targetPath.indexOf(rootFolder) !== 0) {
+				return callback(true);
+			}
+			callback(false, targetPath);
+		}
+	}
 }
 
 module.exports = FilesManager;
---
+++ MimeType.js
@@ -0,0 +1,270 @@
+const extensionsMimeTypes = {
+    "aac": {
+        name: "audio/aac",
+        binary: true
+    },
+    "abw": {
+        name: "application/x-abiword",
+        binary: true
+    },
+    "arc": {
+        name: "application/x-freearc",
+        binary: true
+    },
+    "avi": {
+        name: "video/x-msvideo",
+        binary: true
+    },
+    "azw": {
+        name: "application/vnd.amazon.ebook",
+        binary: true
+    },
+    "bin": {
+        name: "application/octet-stream",
+        binary: true
+    }, "bmp": {
+        name: "image/bmp",
+        binary: true
+    }, "bz": {
+        name: "application/x-bzip",
+        binary: true
+    }, "bz2": {
+        name: "application/x-bzip2",
+        binary: true
+    }, "csh": {
+        name: "application/x-csh",
+        binary: false
+    }, "css": {
+        name: "text/css",
+        binary: false
+    }, "csv": {
+        name: "text/csv",
+        binary: false
+    }, "doc": {
+        name: "application/msword",
+        binary: true
+    }, "docx": {
+        name: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        binary: true
+    }, "eot": {
+        name: "application/vnd.ms-fontobject",
+        binary: true
+    }, "epub": {
+        name: "application/epub+zip",
+        binary: true
+    }, "gz": {
+        name: "application/gzip",
+        binary: true
+    }, "gif": {
+        name: "image/gif",
+        binary: true
+    }, "htm": {
+        name: "text/html",
+        binary: false
+    }, "html": {
+        name: "text/html",
+        binary: false
+    }, "ico": {
+        name: "image/vnd.microsoft.icon",
+        binary: true
+    }, "ics": {
+        name: "text/calendar",
+        binary: false
+    }, "jpeg": {
+        name: "image/jpeg",
+        binary: true
+    }, "jpg": {
+        name: "image/jpeg",
+        binary: true
+    }, "js": {
+        name: "text/javascript",
+        binary: false
+    }, "json": {
+        name: "application/json",
+        binary: false
+    }, "jsonld": {
+        name: "application/ld+json",
+        binary: false
+    }, "mid": {
+        name: "audio/midi",
+        binary: true
+    }, "midi": {
+        name: "audio/midi",
+        binary: true
+    }, "mjs": {
+        name: "text/javascript",
+        binary: false
+    }, "mp3": {
+        name: "audio/mpeg",
+        binary: true
+    }, "mpeg": {
+        name: "video/mpeg",
+        binary: true
+    }, "mpkg": {
+        name: "application/vnd.apple.installer+xm",
+        binary: true
+    }, "odp": {
+        name: "application/vnd.oasis.opendocument.presentation",
+        binary: true
+    }, "ods": {
+        name: "application/vnd.oasis.opendocument.spreadsheet",
+        binary: true
+    }, "odt": {
+        name: "application/vnd.oasis.opendocument.text",
+        binary: true
+    }, "oga": {
+        name: "audio/ogg",
+        binary: true
+    },
+    "ogv": {
+        name: "video/ogg",
+        binary: true
+    },
+    "ogx": {
+        name: "application/ogg",
+        binary: true
+    },
+    "opus": {
+        name: "audio/opus",
+        binary: true
+    },
+    "otf": {
+        name: "font/otf",
+        binary: true
+    },
+    "png": {
+        name: "image/png",
+        binary: true
+    },
+    "pdf": {
+        name: "application/pdf",
+        binary: true
+    },
+    "php": {
+        name: "application/php",
+        binary: false
+    },
+    "ppt": {
+        name: "application/vnd.ms-powerpoint",
+        binary: true
+    },
+    "pptx": {
+        name: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        binary: true
+    },
+    "rtf": {
+        name: "application/rtf",
+        binary: true
+    },
+    "sh": {
+        name: "application/x-sh",
+        binary: false
+    },
+    "svg": {
+        name: "image/svg+xml",
+        binary: false
+    },
+    "swf": {
+        name: "application/x-shockwave-flash",
+        binary: true
+    },
+    "tar": {
+        name: "application/x-tar",
+        binary: true
+    },
+    "tif": {
+        name: "image/tiff",
+        binary: true
+    },
+    "tiff": {
+        name: "image/tiff",
+        binary: true
+    },
+    "ts": {
+        name: "video/mp2t",
+        binary: true
+    },
+    "ttf": {
+        name: "font/ttf",
+        binary: true
+    },
+    "txt": {
+        name: "text/plain",
+        binary: false
+    },
+    "vsd": {
+        name: "application/vnd.visio",
+        binary: true
+    },
+    "wav": {
+        name: "audio/wav",
+        binary: true
+    },
+    "weba": {
+        name: "audio/webm",
+        binary: true
+    },
+    "webm": {
+        name: "video/webm",
+        binary: true
+    },
+    "webp": {
+        name: "image/webp",
+        binary: true
+    },
+    "woff": {
+        name: "font/woff",
+        binary: true
+    },
+    "woff2": {
+        name: "font/woff2",
+        binary: true
+    },
+    "xhtml": {
+        name: "application/xhtml+xml",
+        binary: false
+    },
+    "xls": {
+        name: "application/vnd.ms-excel",
+        binary: true
+    },
+    "xlsx": {
+        name: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        binary: true
+    },
+    "xml": {
+        name: "text/xml",
+        binary: false
+    },
+    "xul": {
+        name: "application/vnd.mozilla.xul+xml",
+        binary: true
+    },
+    "zip": {
+        name: "application/zip",
+        binary: true
+    },
+    "3gp": {
+        name: "video/3gpp",
+        binary: true
+    },
+    "3g2": {
+        name: "video/3gpp2",
+        binary: true
+    },
+    "7z": {
+        name: "application/x-7z-compressed",
+        binary: true
+    }
+};
+
+const defaultMimeType = {
+    name: "text/plain",
+    binary: false
+};
+module.exports.getMimeTypeFromExtension = function (extension) {
+    if (typeof extensionsMimeTypes[extension] !== "undefined") {
+        return extensionsMimeTypes[extension];
+    }
+    return defaultMimeType;
+};
