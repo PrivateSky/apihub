6b76b83ece1fa4e786fa7cc7fbae27f20c5412ff
skutner <sava.dumitru.daniel@gmail.com>
2020-05-14 15:50:17 +0300

    modified ServerConfig for an easy configuration using server.json

--- ServerConfig.js
+++ ServerConfig.js
@@ -5,10 +5,10 @@ function ServerConfig(conf) {
         "port": 8080,
         "zeromqForwardAddress": "tcp://127.0.0.1:5001",
         "preventRateLimit": false,
-        "endpoints":["virtualMQ", "filesManager", "edfs", "dossier-wizard"],
+        "endpoints": ["virtualMQ", "filesManager", "edfs", "dossier-wizard"],
         "endpointsConfig": {
             "virtualMQ": {
-                "path":"./ChannelsManager.js",
+                "path": "./modules/psk-webserver/ChannelsManager.js",
                 "channelsFolderName": "channels",
                 "maxSize": 100,
                 "tokenSize": 48,
@@ -17,18 +17,48 @@ function ServerConfig(conf) {
                 "enableSignatureCheck": true
             },
             "dossier-wizard": {
-                "path":"dossier-wizard"
+                "path": "dossier-wizard"
             },
             "edfs": {
-                "path":"edfs-middleware"
+                "path": "edfs-middleware"
             },
             "filesManager": {
-                "path":"./FilesManager.js"
+                "path": "./modules/psk-webserver/FilesManager.js"
             }
         }
     };
-    conf = conf || {};
-    conf = Object.assign(defaultConf, conf);
+
+    function createConfig(config, defaultConfig) {
+        if (typeof config === "undefined") {
+            return defaultConfig;
+        }
+
+        //ensure that the config object will contain all the necessary keys for server configuration
+        for (let mandatoryKey in defaultConfig) {
+            if (typeof config[mandatoryKey] === "undefined") {
+                config[mandatoryKey] = defaultConfig[mandatoryKey];
+            }
+        }
+        return __createConfigRecursively(conf, defaultConf);
+
+        function __createConfigRecursively(config, defaultConfig) {
+            for (let prop in defaultConfig) {
+                if (typeof config[prop] === "object" && !Array.isArray(config[prop])) {
+                    __createConfigRecursively(config[prop], defaultConfig[prop]);
+                } else {
+                    if (typeof config[prop] === "undefined") {
+                        //only non-object values and arrays in defaultConfig are copied in config
+                        if (typeof defaultConfig[prop] !== "object" || Array.isArray(defaultConfig[prop])) {
+                            config[prop] = defaultConfig[prop];
+                        }
+                    }
+                }
+            }
+            return config;
+        }
+    }
+
+    conf = createConfig(conf, defaultConf);
     return conf;
 }
 
