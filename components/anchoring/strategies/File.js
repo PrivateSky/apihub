8ce9feb1e5ec9057d6750fa88dd7ca831578ba61
Paul Isache <paul@isache.com>
2020-09-17 21:43:22 +0300

    Refactor name

--- components/anchoring/strategies/File.js
+++ components/anchoring/strategies/File.js
@@ -6,7 +6,7 @@ const ALIAS_SYNC_ERR_CODE = 'sync-error';
 
 let anchorsFolders;
 
-$$.flow.describe('AnchorsManager', {
+$$.flow.describe('File', {
     init: function (rootFolder, folderName) {
         const storageFolder = path.join(rootFolder || server.rootFolder, folderName || 'anchors');
         anchorsFolders = path.resolve(storageFolder);
