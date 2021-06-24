e3e19b1bc6b102c742205697c623dbeb8fe0283e
skutner <sava.dumitru.daniel@gmail.com>
2021-06-24 15:42:54 +0300

    small fix

--- components/mqHub/index.js
+++ components/mqHub/index.js
@@ -44,7 +44,7 @@ function MQHub(server) {
 	}
 
 	function allowUnregisteredDID(domainName){
-		const domainConfig = config.getDomainConfig(request.params.domain);
+		const domainConfig = config.getDomainConfig(domainName);
 		let allowUnregisteredDID = defaultSettings.mq_allow_unregistered_did;
 		if(domainConfig && typeof domainConfig.mq_allow_unregistered_did !== undefined){
 			allowUnregisteredDID = !!domainConfig.mq_allow_unregistered_did;
