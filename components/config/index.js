96463008b94807f279fcec272acf5678db2cc000
Vlad Balmos <vlad.balmos@gmail.com>
2021-06-26 18:55:06 +0300

    Catch unhandled exceptions in the config middleware that could crash the server

--- components/config/index.js
+++ components/config/index.js
@@ -5,7 +5,13 @@ function Config(server) {
 
     function getDomainConfig(request, response) {
         const { domain } = request.params;
-        const domainConfig = config.getDomainConfig(domain);
+        let domainConfig;
+        try {
+            domainConfig = config.getDomainConfig(domain);
+        } catch (e) {
+            console.error(e);
+            return response.send(500, "Unable to fetch domain configuration");
+        }
         response.send(200, domainConfig);
     }
 
@@ -19,12 +25,17 @@ function Config(server) {
     function updateDomainConfig(request, response) {
         const { domain } = request.params;
         const domainConfig = request.body;
-        config.updateDomainConfig(domain, domainConfig, (error) => {
-            if (error) {
-                return response.send(500, error);
-            }
-            response.send(200);
-        });
+        try {
+            config.updateDomainConfig(domain, domainConfig, (error) => {
+                if (error) {
+                    return response.send(500, error);
+                }
+                response.send(200);
+            });
+        } catch (e) {
+            console.error(e);
+            response.send(500, "Unable to update domain configuration");
+        }
     }
 
     server.use(`/config/:domain/*`, responseModifierMiddleware);
