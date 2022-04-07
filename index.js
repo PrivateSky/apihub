d92ae4069badfae024f52fab8dd6eaecfc0bbf66
skutner <sava.dumitru.daniel@gmail.com>
2022-04-07 10:00:31 +0300

    fixed #638

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function HttpServer({ listeningPort, rootFolder, sslConfig, dynamicPort, restart
 		server.use(function (req, res, next) {
 			res.setHeader('Access-Control-Allow-Origin', req.headers.origin || req.headers.host);
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-			res.setHeader('Access-Control-Allow-Headers', `Content-Type, Content-Length, X-Content-Length, Access-Control-Allow-Origin, ${conf.componentsConfig.virtualMQ.signatureHeaderName}`);
+			res.setHeader('Access-Control-Allow-Headers', `Content-Type, Content-Length, X-Content-Length, Access-Control-Allow-Origin, ${conf.componentsConfig.virtualMQ.signatureHeaderName}, token`);
 			res.setHeader('Access-Control-Allow-Credentials', true);
 			next();
 		});
