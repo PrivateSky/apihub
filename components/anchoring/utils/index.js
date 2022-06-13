05b0c0d6897dfca7e0f9868884bc5735735d4f50
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-06-13 11:44:25 +0300

    bug fixing typo in anchoring

--- components/anchoring/utils/index.js
+++ components/anchoring/utils/index.js
@@ -11,7 +11,7 @@ const getAnchoringDomainConfig = async (domain) => {
             let adminService = require("./../../admin").getAdminService();
             const getDomainInfo = $$.promisify(adminService.getDomainInfo);
             let domainInfo = await getDomainInfo(domain);
-            if(domainInfo && domain.active && domainInfo.cloneFromDomain){
+            if(domainInfo && domainInfo.active && domainInfo.cloneFromDomain){
                 const clonedDomainConfiguration = config.getDomainConfig(domainInfo.cloneFromDomain);
                 domainConfiguration = clonedDomainConfiguration;
                 console.log(`Config for domain '${domain}' was loaded from admin service.`);
